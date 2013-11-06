9637c7b3b18c32ad276f04beadfa76cb10b19f5a
Navid Nourani-Vatani <navid@nourani.dk>
2013-11-06 11:25:43 +1100

    updated the way hierarchical F1 is calculated. this previous version was incorrect!!

--- nodeCalcF1.m
+++ nodeCalcF1.m
@@ -1,4 +1,4 @@
-function f1 = nodeCalcF1( gt, est, mod )
+function f1 = nodeCalcF1( tree, gt, est, mod )
 % f1 = nodeCalcF1( gt, est )
 % Calculates the hierarchical f1-score by comparing a grount truth sparse
 % matrix label structure to an estimated sparse matrix
@@ -19,18 +19,64 @@ function f1 = nodeCalcF1( gt, est, mod )
 if nargin < 3
     mod = false;
 end
+% disp(gt')
+% disp(est')
 
 tp = sum( and(gt, est) );
-
 if mod == false
     fpfn = sum( xor(gt, est) );
 else
-    endi = find(gt>0, 1, 'last');
+    endi = length(gt);
+    nGT = find(gt>0, 1, 'last')-1; % -1 because first node is 0
+    nEst = find(est>0, 1, 'last')-1;
+    if isParent(tree, nGT) && isChield(tree, nGT, nEst)
+        endi = nGT;
+    end
+    
     fpfn = 0;
     for i = 1:endi
         if gt(i) ~= est(i), fpfn = fpfn+1; end
     end
 end
-f1 = 2*tp / (2*tp+fpfn);
 
+f1 = 2*tp / (2*tp+fpfn);
+% fprintf( 'tp=%d, fpfn=%d => f1=%.2f\n', tp, fpfn, f1)
+
+%% helper functions
+    function p = isParent(tree, n)
+        % Is n a parent node?
+        node = nodeAt( tree, n );
+        p = ~isempty( node.children );
+%         if p == true
+%             fprintf('node %d is a parent\n', node.num)
+%         end
+    end
+
+    function c = isChield(tree, n1, n2)
+        c = false;
+        % Is the n2 and chield of n1?
+        node1 = nodeAt(tree, n1);
+        node2 = nodeAt(tree, n2);
+%         fprintf( 'is node %d child of node %d?\n', node2.num, node1.num )
+        % go through all the child nodes of n1 and see if n2 is there
+        cList = {};
+        for i = 1:length(node1.children)
+            cList{end+1} = node1.children{i};
+        end
+        while ~isempty(cList)
+            if cList{1}.num == node2.num
+%                 fprintf('\tYES!\n')
+                c = true;
+                break;
+            end
+%             fprintf( '\tNot node %d\n', cList{1}.num )
+            
+            % add child nodes and delete current node
+            for i = 1:length(cList{1}.children)
+                cList{end+1} = cList{1}.children{i};
+            end
+            cList = cList(2:end);
+        end
+    end
+    
 end
